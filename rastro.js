34d1ec8371f65c5d56f1cff9139edef2843fd03d
Fernando Agostinho <agostinho.ufsc@gmail.com>
2016-04-01 15:21:11 -0300

    Update rastro.js

--- rastro.js
+++ rastro.js
@@ -3,3 +3,7 @@
 	document.getElementByXpath("//*[@id='1-1-identificação-do-cidadão']").style.color = "#4C9900";
 
 }).call(this);
+
+function getElementByXpath(path) {
+  return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
